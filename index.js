f3188662a408eb72b8e97081ff041955948ea6f5
PolovkaOleksandr <120940683+PolovkaOleksandr@users.noreply.github.com>
2024-09-25 17:47:20 +0300

    2

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const inp11=document.querySelector(".inp11")
-const inp12=document.querySelector(".inp12")
-const inp13=document.querySelector(".inp13")
+const inp1=document.querySelector(".inp1")
 
-const inp21=document.querySelector(".inp21")
-const inp22=document.querySelector(".inp22")
-const inp23=document.querySelector(".inp23")
+
+const inp2=document.querySelector(".inp2")
+
 
 const btn=document.querySelector(".btn")
 btn.addEventListener("click", main)
@@ -15,17 +13,26 @@ let years=0
 let months=0
 let days=0
 
+
+
+
 function main() {
-    years=inp23.value-inp13.value
-    months=inp22.value-inp12.value
-    days=inp21.value-inp11.value
-    if (months<0){
-        years=years-1
-        months=months+12
-    }
+    let inv1 = inp1.value.split('.').map(Number);
+    let inv2 = inp2.value.split('.').map(Number);
+    years=inv2[2]-inv1[2]
+    months=inv2[1]-inv1[1]
+    days=inv2[0]-inv1[0]
     if (days<0){
         months=months-1
-        days=days+30
+        days=30+days
     }
+    if (months<0){
+        years=years-1
+        months=12+months
+    }
+    
+    
     p.textContent=`${days} днів ${months} місяців ${years} років`
+    console.log(inv1, inv2, years)
+    console.log(inv2[3],inv1[3])
 }
